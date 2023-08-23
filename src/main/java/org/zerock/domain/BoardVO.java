ee05685c458cf3b4e93d8117a9e219d01d500b73
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-23 15:45:10 +0900

    BoardVO 작성

---
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+	private long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updatedate;
+}
