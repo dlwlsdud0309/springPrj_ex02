afc33f77dcaf48272e61614e49a51f480156683a
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-25 15:01:39 +0900

    jex02의 BoardMapper와 ex02의 BoardMapper를 착각.. 다시 주석처리하여 테스트 정상 실행 확인, order by 사용한 쿼리 select * from tbl_board order by bno desc; 로도 정상 실행 확인

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -9,6 +9,6 @@ public interface BoardMapper {
 
 	//Mapper 인터페이스를 작성할 때는 리스트와 등록 작업을 우선해서 작성함
 	
-	@Select("select * from tbl_board where bno > 0")
+//	@Select("select * from tbl_board where bno > 0")  //XML에 SQL문을 작성하였으니 주석처리
 	public List<BoardVO> getList();
 }
