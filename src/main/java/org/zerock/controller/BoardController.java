e7d16175db221e08e85f5e3f32dd5394c707299f
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-09-04 12:25:12 +0900

    BoardController에서 전체 목록을 가져오는 처리를 먼저 작성, BoardController는 BoardService에 대해서 의존적이므로 BoardService 타입의 객체와 같이 연동해야 하기 때문에 의존성에 대한 처리도 함께 진행한다. @AllArgsContructor를 이용해 생성자를 만들고 자동으로 주입하도록 함(생성자를 만들지 않을 경우 @Setter(onMethod_={@Autowired}) 이용하여 처리)

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -1,13 +1,25 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.service.BoardService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping("/board/*")
-public class BoardController {
+@AllArgsConstructor //생성자를 만들지 않을 경우 @Setter(onMethod_={@Autowired}) 이용하여 처리
+public class BoardController { //BoardController는 BoardService에 대해서 의존적이다
 
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
 }
