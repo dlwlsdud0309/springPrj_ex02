54f66b0a062a0550986cc22ca6ef1c79ced5fc17
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-09-08 13:23:54 +0900

    삭제 테스트 코드 정상 실행 확인

--- src/test/java/org/zerock/controller/BoardControllerTests.java
+++ src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -83,4 +83,14 @@ public class BoardControllerTests {
 	
 		log.info(resultPage);
 	}
+	
+	@Test
+	public void testRemove() throws Exception{
+		
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/remove")
+						.param("bno", "21")
+				).andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
 }
