0159c11c353a48653560d8f1276673e0fea98722
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-05 17:34:52 +0900

    페이징 처리는 브라우저에서 들어오는 정보들을 기준으로 동작하기 때문에 BoardController와 BoardService를 전달되는 파라미터들을 받는 형태로 수정해야한다

--- src/main/java/org/zerock/service/BoardService.java
+++ src/main/java/org/zerock/service/BoardService.java
@@ -3,6 +3,7 @@ package org.zerock.service;
 import java.util.List;
 
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 public interface BoardService {
 
@@ -14,5 +15,9 @@ public interface BoardService {
 	
 	public boolean remove(Long bno);
 	
-	public List<BoardVO> getList();
+//	public List<BoardVO> getList();
+	
+	//페이징 처리는 브라우저에서 들어오는 정보들을 기준으로 동작
+	//BoardController와 BoardService를 전달되는 파라미터들을 받는 형태로 수정해야한다
+	public List<BoardVO> getList(Criteria cri);
 }
