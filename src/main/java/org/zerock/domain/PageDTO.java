61b23b02d13fe71f039d543217ee5673cf4120de
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-07 12:35:53 +0900

    페이징 화면 처리 위해 PageDTO 클래스 생성

---
+++ src/main/java/org/zerock/domain/PageDTO.java
@@ -0,0 +1,9 @@
+package org.zerock.domain;
+
+public class PageDTO {
+	// 페이징 처리할 떄 필요한 정보들
+	// 1.현제 페이지 번호(page)
+	// 2.이전과 다음으로 이동 가능한 링크의 표시 여부(prev, next)
+	// 3. 화면에서 보여지는 페이지의 시작 번호와 끝 번호(startPage, endPage)
+	
+}
